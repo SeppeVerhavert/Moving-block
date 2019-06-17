eb7c3678c7baf3842d860ad10b2cf79a3e61972c
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-17 16:14:14 +0200

    add left, right, top, down

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-var square = document.getElementById('square');
-var container = document.getElementById('container');
+const square = document.getElementById('square');
+const container = document.getElementById('container');
+
+let squareLeft = 0;
+let squareDown = 0;
 
 function animate(e) {
-    alert(e.keyCode);
+    console.log(e.keyCode);
 
-    if (e.keyCode === 39){
-        console.log("left")
+    if (e.keyCode === 39) {
+        squareLeft += 100;
+        square.style.left = squareLeft + 'px';
     }
 
-    if (e.keyCode === 37){
-        console.log("right")
+    if (e.keyCode === 37) {
+        squareLeft -= 100;
+        square.style.left = squareLeft + 'px';
+    }
+
+    if (e.keyCode === 40) {
+        squareDown += 100;
+        square.style.top = squareDown + 'px';
+    }
+
+    if (e.keyCode === 38) {
+        squareDown -= 100;
+        square.style.top = squareDown + 'px';
     }
 }
 
