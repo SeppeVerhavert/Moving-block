00f6984d1c248468e775f67ffe2ad54aa0af8f9e
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-18 09:02:48 +0200

    fix boundaries container

--- main.js
+++ main.js
@@ -12,24 +12,24 @@ function animate(e) {
     if (e.keyCode === 39) {
         squareLeft += 50;
         square.style.left = squareLeft + 'px';
-        if (squareLeft >= 450) {
-            squareLeft = 00;
+        if (squareLeft >= 400) {
+            squareLeft = 400;
         }
     }
 
     if (e.keyCode === 37) {
         squareLeft -= 50;
         square.style.left = squareLeft + 'px';
-        if (squareLeft < 0) {
-            squareLeft = 500;
+        if (squareLeft < 50) {
+            squareLeft = 50;
         }
     }
 
     if (e.keyCode === 40) {
         squareTop += 50;
         square.style.top = squareTop + 'px';
-        if (squareTop > 450) {
-            squareTop = 0;
+        if (squareTop > 400) {
+            squareTop = 400;
         }
         
     }
@@ -37,8 +37,8 @@ function animate(e) {
     if (e.keyCode === 38) {
         squareTop -= 50;
         square.style.top = squareTop + 'px';
-        if (squareTop < 0) {
-            squareTop = 500;
+        if (squareTop < 50) {
+            squareTop = 50;
         }
     }
 }
