17d44b89249f86ec6fe162d8d72cbfc7fc306087
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-18 11:49:59 +0200

    add counter on food

--- main.js
+++ main.js
@@ -2,35 +2,12 @@ const square = document.getElementById('square');
 const container = document.getElementById('container');
 const food = document.createElement('div');
 
+document.onkeydown = animate;
 document.getElementById("body").addEventListener("load", createFood());
 
 let squareLeft = 0;
 let squareTop = 0;
-
-function createFood() {
-
-    food.id = 'food';
-    
-    randomX = Math.floor(Math.random() * (8) + 1)*50;
-    randomY = Math.floor(Math.random() * (8) + 1)*50;
-
-    food.style.left = randomX + 'px';
-    food.style.top = randomY + 'px';
-
-    container.appendChild(food);
-}
-
-function deleteFood() {
-    food.parentNode.removeChild(food);
-}
-
-function detectCollision() {
-    if (squareLeft === randomX && squareTop === randomY){
-        deleteFood();
-        createFood();
-    }
-    
-}
+let counter = 0;
 
 function animate(e) {
 
@@ -69,4 +46,32 @@ function animate(e) {
     detectCollision();
 }
 
-document.onkeydown = animate;
+function createFood() {
+
+    food.id = 'food';
+    
+    randomX = Math.floor(Math.random() * (8) + 1)*50;
+    randomY = Math.floor(Math.random() * (8) + 1)*50;
+
+    food.style.left = randomX + 'px';
+    food.style.top = randomY + 'px';
+
+    container.appendChild(food);
+}
+
+function deleteFood() {
+    food.parentNode.removeChild(food);
+}
+
+function detectCollision() {
+    if (squareLeft === randomX && squareTop === randomY){
+        deleteFood();
+        keepCount();
+        createFood();
+    }
+}
+
+function keepCount() {
+    ++counter;
+    document.getElementById('counter').innerHTML = counter;
+}
